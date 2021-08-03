2c38363908fbc3e9a04f978787c60f13edca0edb
andrew <neon.duncan@gmail.com>
2021-08-03 16:51:53 -0500

    commit tree 07893e1ee91d5b444ef92a1ce35daa231017a6ef

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    new_word = @word.chars.sort {|a,b| a <=> b}
+    anagram = []
+    array.each do |w|
+      if w.chars.sort {|a,b| a <=> b} == new_word
+      anagram.push(w)
+    end
+  end
+  anagram
+  # binding.pry
+  end
+
+end
+
+
+#listen char
+#string.each |char| 
